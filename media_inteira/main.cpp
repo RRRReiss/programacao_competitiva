2ab5b3e159fc5f89d49633dca2bcfe231d5bc714
Rafael Reis <105391575+RRRReiss@users.noreply.github.com>
2025-06-27 18:01:58 -0300

    exercicios 1 e 2 feitos

---
+++ media_inteira/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int A, B, media;
+
+    cin >> A >> B;
+
+    media = (A + B)/2;
+
+    cout << media << endl;
+
+    return 0;
+}
---
+++ ola_neps_academy/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Ola Neps Academy!";
+
+    return 0;
+}
