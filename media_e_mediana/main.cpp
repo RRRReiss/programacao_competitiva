03e2422fafc2d50d813f5abf620caf1adf6713d4
Rafael Reis <105391575+RRRReiss@users.noreply.github.com>
2025-06-27 19:13:22 -0300

    exercicio 6 feito

---
+++ media_e_mediana/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int A, B, C, media;
+
+    cin >> A >> B;
+
+    media = A;
+
+    C = 3*media - A - B; 
+
+    cout << C;
+
+    return 0;
+}
