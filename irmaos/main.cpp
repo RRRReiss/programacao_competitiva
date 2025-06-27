e7de46db42b33771d66b8f63b1c37031e4444bef
Rafael Reis <105391575+RRRReiss@users.noreply.github.com>
2025-06-27 18:49:57 -0300

    exercicio 4 feito

---
+++ irmaos/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int orlando, otavio, oscar, diferenca_idade;
+
+    cin >> oscar >> otavio;
+    
+    diferenca_idade = otavio-oscar;
+
+    orlando = diferenca_idade + otavio;
+
+    cout << orlando;
+    return 0;
+}
